aa3253f30c5971ef2d68390278a0a0f93088b131
Jkat <fission@live.com>
2015-06-03 18:01:51 -0400

    Adding comments to CoucDB deletion script

--- tools/CoucDB_Delete_Documents.php
+++ tools/CoucDB_Delete_Documents.php
@@ -1,5 +1,7 @@
 <?php
 
+//first draft, should take parameters instead of the hardcoded 'instruments','tsi' instrument name, etc
+
 $result = file_get_contents('http://localhost:5984/prevent_ad_dqg/_design/DQG-2.0/_view/instruments?startkey=["tsi","MTL0000","000000"]&endkey=["tsi","MTL9999","ZZZZZZ"]&reduce=false');
 $json = json_decode($result);
 $json = $json->rows;
