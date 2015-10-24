9e860a83b2c87131bcd392a5b5b31c8fb62157d2
Jkat <fission@live.com>
2015-10-24 15:13:22 -0400

    Calling Drug Compliance calculation algorithm after ajax update

--- modules/drug_compliance/ajax/update_drug_compliance.php
+++ modules/drug_compliance/ajax/update_drug_compliance.php
@@ -14,6 +14,7 @@ require_once 'NDB_Client.class.inc';
 $config =& NDB_Config::singleton();
 $client = new NDB_Client();
 $client->initialize();
+require_once 'NDB_Form_drug_compliance.class.inc';
 
 if (get_magic_quotes_gpc()) {
     // Magic quotes adds \ to description, get rid of it.
@@ -38,6 +39,8 @@ if ($user->hasPermission('data_entry')) { //if user has data entry permission
     //only allow certain fields to be updated
     if ($field == 'drug_issued_date' || $field == 'pills_issued' || $field == 'drug_returned_date' || $field == 'pills_returned') {
         $DB->update('drug_compliance', array($field=>$value), array('ID'=>$id));
+        $candid = $DB->pselectOne("SELECT candid from drug_compliance where ID=:dcid", array('dcid'=>$id));
+        NDB_Form_drug_compliance::calculate($candid, $id);
     }
 }
 
