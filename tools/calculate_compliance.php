b4d2307160654afa5b91e8c9fa991514cd0d01a9
Jkat <fission@live.com>
2016-06-14 15:26:33 -0400

    Fixing calculate compliance script

--- tools/calculate_compliance.php
+++ tools/calculate_compliance.php
@@ -2,11 +2,7 @@
 
 set_include_path(get_include_path().":../project/libraries:../php/libraries:../modules/drug_compliance/php:");
 require_once __DIR__ . "/../vendor/autoload.php";
-
-require_once "NDB_Client.class.inc";
-$client = new NDB_Client();
-$client->makeCommandLine();
-$client->initialize("../project/config.xml");
+require_once "generic_includes.php";
 
 require_once "NDB_Form_drug_compliance.class.inc";
 NDB_Form_drug_compliance::calculate();
