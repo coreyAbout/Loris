4f2569904b5ff21808b45d8a5c7452ca698ca564
Jkat <fission@live.com>
2016-06-19 01:25:44 -0400

    Putting more explicit path for recalculate compliance script

--- tools/calculate_compliance.php
+++ tools/calculate_compliance.php
@@ -1,13 +1,13 @@
 <?php
 
-set_include_path(get_include_path().":../project/libraries:../php/libraries:../modules/drug_compliance/php/:");
+set_include_path(get_include_path().":../project/libraries:../php/libraries:");
 require_once __DIR__ . "/../vendor/autoload.php";
 require_once "NDB_Client.class.inc";
 $client = new NDB_Client();
 $client->makeCommandLine();
 $client->initialize();
 
-require_once "NDB_Form_drug_compliance.class.inc";
+require_once __DIR__ . "/../modules/drug_compliance/php/NDB_Form_drug_compliance.class.inc";
 NDB_Form_drug_compliance::calculate();
 
 ?>
