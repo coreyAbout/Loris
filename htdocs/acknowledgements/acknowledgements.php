200438d2814264088b92de56dc816cb39a763749
Jkat <fission@live.com>
2016-09-18 16:12:01 -0400

    Adding public facing Acknowledgements display

---
+++ htdocs/acknowledgements/acknowledgements.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * Publicly available generator for acknowledgements
+ *
+ * PHP Version 5
+ *
+ * @category Loris
+ * @package  Behavioral
+ * @author   Justin Kat <justin.kat@mail.mcgill.ca>
+ * @license  http://www.gnu.org/licenses/gpl-3.0.txt GPLv3
+ * @link     https://github.com/aces/Loris
+ */
+
+require_once __DIR__ . "/../../vendor/autoload.php";
+
+$client = new NDB_Client();
+$client->makeCommandLine();
+$client->initialize();
+
+$config = NDB_Config::singleton();
+$db     = Database::singleton();
+
+$factory = NDB_Factory::singleton();
+$baseurl = $factory->settings()->getBaseURL();
+
+$css = $config->getSetting('css');
+
+$publication_date = $_GET["date"];
+
+$columns = array(
+            full_name     => 'Full Name',
+            citation_name => 'Citation Name',
+            affiliations  => 'Affiliations',
+            degrees       => 'Degrees',
+            roles         => 'Roles',
+            present       => 'Present?',
+           );
+
+$keysAsString             = implode(', ', array_keys($columns));
+
+$results = $db->pselect(
+    "SELECT " . $keysAsString .
+    " FROM acknowledgements
+    WHERE start_date <= :publication_date
+    AND (DATEDIFF(end_date,start_date) > 90 OR end_date='0000-00-00')
+    ORDER BY ordering ASC",
+    array('publication_date' => $publication_date)
+);
+
+$tpl_data['baseurl'] = $config->getSetting('url');
+$tpl_data['css']     = $config->getSetting('css');
+$tpl_data['columns'] = $columns;
+$tpl_data['results'] = $results;
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $CSVheaders = array();
+    $CSVdata    = array();
+
+    foreach ($columns as $k => $v) {
+        array_push($CSVheaders, $v);
+    }
+    foreach ($results as $k => $v) {
+        array_push($CSVdata, $v);
+    }
+
+    $toCSV['headers'] = $CSVheaders;
+    $toCSV['data']    = $CSVdata;
+
+    $CSV = Utility::arrayToCSV($toCSV);
+
+    header("Content-Type: text/plain");
+    header('Content-Disposition: attachment; filename=data.csv');
+    header("Content-Length: " . strlen($CSV));
+    echo $CSV;
+    exit();
+
+}
+
+//Output template using Smarty
+$smarty = new Smarty_neurodb;
+$smarty->assign($tpl_data);
+$smarty->display('acknowledgements.tpl');
+
+?>
