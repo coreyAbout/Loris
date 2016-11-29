d666c61a30f8fced6386001c0e04294d36d245f3
Jkat <fission@live.com>
2016-11-29 13:23:39 -0500

    Updating CSF import script

--- tools/import_biomarkers.php
+++ tools/import_biomarkers.php
@@ -79,6 +79,8 @@ for( $i = 0; $i < sizeof($fixedLines); $i++ )
                                         $visit = 'NAPLP24';
                                 elseif($thisField[$key] == 'F36')
                                         $visit = 'NAPLP36';
+                                elseif($thisField[$key] == 'F48')
+                                        $visit = 'NAPLP48';
 
 				echo "Visit: " . $visit . "\n";
 			}
@@ -141,6 +143,147 @@ for( $i = 0; $i < sizeof($fixedLines); $i++ )
                            }
                         }
 */
+
+                        if ($key == 'EGF') {
+                                $EGF = $thisField[$key];
+                        }
+
+                        if ($key == 'Eotaxin') {
+                                $Eotaxin = $thisField[$key];
+                        }
+
+                        if ($key == 'G-CSF') {
+                                $G_CSF = $thisField[$key];
+                        }
+
+                        if ($key == 'GM-CSF') {
+                                $GM_CSF = $thisField[$key];
+                        }
+
+                        if ($key == 'IFN_alpha_2') {
+                                $IFN_alpha_2 = $thisField[$key];
+                        }
+
+                        if ($key == 'IFNy') {
+                                $IFNy = $thisField[$key];
+                        }
+
+                        if ($key == 'IL-10') {
+                                $IL_10 = $thisField[$key];
+                        }
+
+                        if ($key == 'IL-12P40') {
+                                $IL_12P40 = $thisField[$key];
+                        }
+
+                        if ($key == 'IL-12P70') {
+                                $IL_12P70 = $thisField[$key];
+                        }
+
+                        if ($key == 'IL-13') {
+                                $IL_13 = $thisField[$key];
+                        }
+
+                        if ($key == 'IL-15') {
+                                $IL_15 = $thisField[$key];
+                        }
+
+                        if ($key == 'IL-17') {
+                                $IL_17 = $thisField[$key];
+                        }
+
+                        if ($key == 'IL-1RA') {
+                                $IL_1RA = $thisField[$key];
+                        }
+
+                        if ($key == 'IL-1alpha') {
+                                $IL_1alpha = $thisField[$key];
+                        }
+
+                        if ($key == 'IL-1beta') {
+                                $IL_1beta = $thisField[$key];
+                        }
+
+                        if ($key == 'IL-2') {
+                                $IL_2 = $thisField[$key];
+                        }
+
+                        if ($key == 'IL-3') {
+                                $IL_3 = $thisField[$key];
+                        }
+
+                        if ($key == 'IL-4') {
+                                $IL_4 = $thisField[$key];
+                        }
+
+                        if ($key == 'IL-5') {
+                                $IL_5 = $thisField[$key];
+                        }
+
+                        if ($key == 'IL-6') {
+                                $IL_6 = $thisField[$key];
+                        }
+
+                        if ($key == 'IL-7') {
+                                $IL_7 = $thisField[$key];
+                        }
+
+                        if ($key == 'IL-8') {
+                                $IL_8 = $thisField[$key];
+                        }
+
+                        if ($key == 'IP-10') {
+                                $IP_10 = $thisField[$key];
+                        }
+
+                        if ($key == 'MCP-1') {
+                                $MCP_1 = $thisField[$key];
+                        }
+
+                        if ($key == 'MIP-1alpha') {
+                                $MIP_1alpha = $thisField[$key];
+                        }
+
+                        if ($key == 'MIP-1beta') {
+                                $MIP_1beta = $thisField[$key];
+                        }
+
+                        if ($key == 'TNF_alpha') {
+                                $TNF_alpha = $thisField[$key];
+                        }
+
+                        if ($key == 'TNF_beta') {
+                                $TNF_beta = $thisField[$key];
+                        }
+
+                        if ($key == 'VEGF') {
+                                $VEGF = $thisField[$key];
+                        }
+
+                        if ($key == '29-plex_date') {
+                                $29_plex_date = $thisField[$key];
+                        }
+
+                        if ($key == 'Abeta34') {
+                                $Abeta34 = $thisField[$key];
+                        }
+
+                        if ($key == 'Abeta34_date') {
+                                $Abeta34_date = $thisField[$key];
+                        }
+
+                        if ($key == 'Abeta40') {
+                                $Abeta40 = $thisField[$key];
+                        }
+
+                        if ($key == 'synuclein') {
+                                $synuclein = $thisField[$key];
+                        }
+
+                        if ($key == 'date_synuclein') {
+                                $date_synuclein = $thisField[$key];
+                        }
+
                 }
 
 
@@ -162,7 +305,8 @@ for( $i = 0; $i < sizeof($fixedLines); $i++ )
 			$DDE_commentID = $db->pselectOne("SELECT CommentID from flag where sessionID=:sid and test_name = 'LP' and CommentID like 'DDE%'", array('sid'=>$sessionID));
 			print "CommentID: " . $commentID ."\n";
 
-			$insertData = array('ELISA_tau'=>$ELISA_tau, 'ELISA_b_amyloid'=>$ELISA_b_amyloid, 'ELISA_ptau'=>$ELISA_ptau, 'ELISA_date'=>$ELISA_date, 'ELISA_NFL'=>$ELISA_NFL, /*'6plex_ApoAI'=>$sixplex_ApoAI, '6plex_ApoAII'=>$sixplex_ApoAII, '6plex_ApoB'=>$sixplex_ApoB, '6plex_ApoCII'=>$sixplex_ApoCII, '6plex_ApoCIII'=>$sixplex_ApoCIII,*/ '6plex_ApoE'=>$sixplex_ApoE, /*'6plex_date'=>$sixplex_date*/);
+			$insertData = array('ELISA_tau'=>$ELISA_tau, 'ELISA_b_amyloid'=>$ELISA_b_amyloid, 'ELISA_ptau'=>$ELISA_ptau, 'ELISA_date'=>$ELISA_date, 'ELISA_NFL'=>$ELISA_NFL, /*'6plex_ApoAI'=>$sixplex_ApoAI, '6plex_ApoAII'=>$sixplex_ApoAII, '6plex_ApoB'=>$sixplex_ApoB, '6plex_ApoCII'=>$sixplex_ApoCII, '6plex_ApoCIII'=>$sixplex_ApoCIII,*/ '6plex_ApoE'=>$sixplex_ApoE, /*'6plex_date'=>$sixplex_date*/, 'EGF'=>$EGF, 'Eotaxin'=>$Eotaxin, 'G_CSF'=>$G_CSF, 'GM_CSF'=>$GM_CSF, 'IFN_alpha_2'=>$IFN_alpha_2, 'IFNy'=>$IFNy, 'IL_10'=>$IL_10, 'IL_12P40'=>$IL_12P40, 'IL_12P70'=>$IL_12P70, 'IL_13'=>$IL_13, 'IL_15'=>$IL_15, 'IL_17'=>$IL_17, 'IL_1RA'=>$IL_1RA, 'IL_1alpha'=>$IL_1alpha, 'IL_1beta'=>$IL_1beta, 'IL_2'=>$IL_2, 'IL_3'=>$IL_3, 'IL_4'=>$IL_4, 'IL_5'=>$IL_5, 'IL_6'=>$IL_6, 'IL_7'=>$IL_7, 'IL_8'=>$IL_8, 'IP_10'=>$IL_10, 'MCP_1'=>$MCP_1, 'MIP_1alpha'=>$MIP_1alpha, 'MIP_1beta'=>$MIP_1beta, 'TNF_alpha'=>$TNF_alpha, 'TNF_beta'=>$TNF_beta, 'VEGF'=>$VEGF, '29_plex_date'=>$29_plex_date, 'Abeta34'=>$Abeta34, 'Abeta34_date'=>$Abeta34_date, 'Abeta40'=>$Abeta40, 'synuclein'=>$synuclein, 'date_synuclein'=>$date_synuclein);
+
 			$where = array('CommentID'=>$commentID);
 			$where2 = array('CommentID'=>$DDE_commentID);
 
