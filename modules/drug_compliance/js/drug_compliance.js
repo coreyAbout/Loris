f4076ef7da1550c37114c49b4dd6ba5726784a5b
Jkat <fission@live.com>
2017-02-14 14:58:30 -0500

    Fixing Drug Compliance javascript to use BaseURL

--- modules/drug_compliance/js/drug_compliance.js
+++ modules/drug_compliance/js/drug_compliance.js
@@ -6,7 +6,7 @@ function save() {
                 id = $(this).closest('tr').attr('id');
                 field = event.target.id;
                 value = $(this).text();
-                $.get("AjaxHelper.php?Module=drug_compliance&script=update_drug_compliance.php&id=" + id + "&field=" + field + "&value=" + value, function(data) {});
+                $.get(loris.BaseURL + "/drug_compliance/ajax/update_drug_compliance.php?id=" + id + "&field=" + field + "&value=" + value, function(data) {});
             }
     ).keypress(function(e) {
         if(e.which === 13) { // Determine if the user pressed the enter button
