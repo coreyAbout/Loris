3c7074dd161cc05f04fccab7ad2af7b47f4f5667
Jkat <601027+Jkat@users.noreply.github.com>
2019-05-29 16:34:50 -0400

    Not removing Final Radiological Review

--- SQL/Release_patches/18.0_To_19.0_upgrade.sql
+++ SQL/Release_patches/18.0_To_19.0_upgrade.sql
@@ -257,12 +257,5 @@ ADD INDEX `index3` (`Chromosome` ASC, `StartLoc` ASC, `EndLoc` ASC, `Strand` ASC
 WARNINGS;
 SET SQL_NOTES=1;
 
-SELECT 'Delete Menu Permission' as 'Step #1';
-DELETE FROM LorisMenuPermissions WHERE MenuID IN
-    (SELECT ID FROM LorisMenu WHERE Link = 'final_radiological_review/');
-
-SELECT 'Delete Menu Entry' as 'Step #2';
-DELETE FROM LorisMenu WHERE Link = 'final_radiological_review/';
-
 SELECT 'Patch complete' as 'Status';
 ALTER TABLE mri_upload CHANGE `Inserting` `Inserting` tinyint(1) DEFAULT NULL;
